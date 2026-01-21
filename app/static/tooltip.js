bbd53c04893b6a6305e2d39f4ed0a4a1bf85fcc3
Alexis <guaniqued@gmail.com>
2026-01-21 04:29:21 -0300

    Se agrego el tooltik al user agent y se sacaron columnas inutiles de las tablas

---
+++ app/static/tooltip.js
@@ -0,0 +1,29 @@
+(() => {
+  // Tooltip con delay al cerrar para poder mover el mouse al panel y scrollear.
+  const CLOSE_DELAY_MS = 350;
+
+  document.querySelectorAll(".tooltip").forEach((el) => {
+    let t = null;
+
+    function open() {
+      if (t) clearTimeout(t);
+      t = null;
+      el.classList.add("tooltip--open");
+    }
+
+    function closeWithDelay() {
+      if (t) clearTimeout(t);
+      t = setTimeout(() => {
+        el.classList.remove("tooltip--open");
+        t = null;
+      }, CLOSE_DELAY_MS);
+    }
+
+    el.addEventListener("mouseenter", open);
+    el.addEventListener("mouseleave", closeWithDelay);
+    el.addEventListener("focusin", open);
+    el.addEventListener("focusout", closeWithDelay);
+  });
+})();
+
+
